f7675831c189376cc33fa61b25cdcff3bc06fcfc
AJAYI OLAYIDE <ebena107@gmail.com>
2021-09-14 23:47:17 +0300

    Initial Commit - Serial Communication added to app Capabilities. Windows Title and Size set in app.xaml

--- Arduino SerialUART/Arduino SerialUART/App.xaml.cs
+++ Arduino SerialUART/Arduino SerialUART/App.xaml.cs
@@ -43,9 +43,24 @@ namespace Arduino_SerialUART
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
             m_window = new MainWindow();
+            IntPtr hwnd = WinRT.Interop.WindowNative.GetWindowHandle(m_window);
+            m_window.Title = "Arduino SerialUART | https://ebena.com.ng";
+                      SetWindowSize(hwnd, 800, 600);
             m_window.Activate();
         }
 
+        private void SetWindowSize(IntPtr hwnd, int width, int height)
+        {
+            // Win32 uses pixels and WinUI 3 uses effective pixels, so you should apply the DPI scale factor
+            var dpi = PInvoke.User32.GetDpiForWindow(hwnd);
+            float scalingFactor = (float)dpi / 96;
+            width = (int)(width * scalingFactor);
+            height = (int)(height * scalingFactor);
+
+            _ = PInvoke.User32.SetWindowPos(hwnd, PInvoke.User32.SpecialWindowHandles.HWND_TOP,
+                                        0, 0, width, height,
+                                        PInvoke.User32.SetWindowPosFlags.SWP_NOMOVE);
+        }
         private Window m_window;
     }
 }
